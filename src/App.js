85f671be32911cd62154667bc42aa38e5491f6d4
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-06-22 19:38:18 +0300

    My job

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+/*import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -8,6 +8,46 @@ function App() {
     <div className="app">
       <NavBar />
       <Outlet />
+    </div>
+  );
+}
+
+export default App;*/
+// App.js
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <div className="app">
+      {isLoggedIn ? (
+        <>
+          <NavBar logout={logout} />
+          <Outlet context={{ login }} />
+        </>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { NavLink} from "react-router-dom";
+/*import { NavLink} from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
@@ -17,6 +17,29 @@ function NavBar() {
       >
         About
       </NavLink>
+    </nav>
+  );
+}
+
+export default NavBar;*/
+
+// NavBar.js
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+function NavBar({ logout }) {
+  return (
+    <nav>
+      <NavLink to="/" className="nav-link">
+        Home
+      </NavLink>
+      <NavLink to="/about" className="nav-link">
+        About
+      </NavLink>
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <NavLink to="/about">About</NavLink>
+      <button onClick={logout}>Logout</button>
     </nav>
   );
 }
